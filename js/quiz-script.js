6ada48b9df8ea22ac74027cbe300af4a9c36705e
Rocco <roccomaniscalco2001@gmail.com>
2020-09-30 03:48:49 -0400

    built displayCompletion() with form to appear upon quiz completion

--- js/quiz-script.js
+++ js/quiz-script.js
@@ -1,64 +1,100 @@
+// initializes DOM variables
 var quizSection = document.querySelector('[name="quiz-section"]');
 var quizHeader = document.querySelector('[name="quiz-header"]');
 
-var randomIndex = 0;
-var questions = ["how", "who", "what"];
-var answerChoices = [
+// initializes global variables
+var questionsAnswered = 0;
+var randomIndex;
+var questions = ["how", "who", "what", "why"];
+var arrOfAnswerChoices = [
   ["me", "him", "her"],
   ["ya", "ye", "yo"],
   ["here", "there", "everywhere"],
+  ["because", "cause", "cus"],
 ];
-var correctAnswers = ["me", "ya", "here"];
+var correctAnswers = ["me", "ya", "here", "because"];
 
+// loads new question and answers
 setRandomIndex();
 displayQuestion();
 displayAnswers();
 
+// sets randomIndex in the range [0,questions.length]
 function setRandomIndex() {
   randomIndex = Math.floor(Math.random() * questions.length);
 }
 
+// displays questions[randomIndex]
 function displayQuestion() {
   var question = document.createElement("h1");
-
-  question.setAttribute("class", "heading");
   question.textContent = questions[randomIndex];
   quizSection.append(question);
 }
 
+// displays answers[randomIndex]
 function displayAnswers() {
-  var answers = answerChoices[randomIndex];
-  console.log(answerChoices[randomIndex]);
+  var answerChoices = arrOfAnswerChoices[randomIndex];
 
-  for (var i = 0; i < answers.length; i++) {
+  for (var i = 0; i < answerChoices.length; i++) {
     var answer = document.createElement("button");
     answer.setAttribute("class", "block");
-    answer.textContent = answers[i];
+    answer.textContent = answerChoices[i];
     quizSection.append(answer);
   }
 }
 
-quizSection.addEventListener("click", function (event) {
-  var feedback = document.createElement("caption");
+// displays quiz completion
+function displayCompletion() {
+  var completionMessage = document.createElement("h1");
+  completionMessage.textContent = "All done!";
+  quizSection.append(completionMessage);
 
+  var finalScoreMessage = document.createElement("p");
+  finalScoreMessage.textContent = "Your score: "; //*********//
+  quizSection.append(finalScoreMessage);
+
+  var initialsForm = document.createElement("form");
+  var initialsInput = document.createElement("input");
+  var initialsLabel = document.createElement("label");
+  var initialsSubmit = document.createElement("input");
+
+  initialsForm.setAttribute("action","./score.html")
+  initialsLabel.textContent = "Enter initials: "
+  initialsSubmit.setAttribute("type", "submit");
+
+  initialsForm.append(initialsLabel, initialsInput, initialsSubmit);
+  quizSection.append(initialsForm);
+}
+
+// upon clicking an answer button...
+quizSection.addEventListener("click", function (event) {
   if (event.target.matches("button")) {
+    
+    // defines feedback caption ("Correct!" or "Wrong!")
+    var feedback = document.createElement("caption");
     if (event.target.textContent === correctAnswers[randomIndex])
       feedback.textContent = "Correct!";
     else feedback.textContent = "Wrong!";
+
+    // removes used question, answers, and correct answer and clears quizSection
+    questions.splice(randomIndex, 1);
+    arrOfAnswerChoices.splice(randomIndex, 1);
+    correctAnswers.splice(randomIndex, 1);
+    quizSection.innerHTML = "";
+
+    // displays quiz completion or loads new question and answers
+    questionsAnswered++;
+    if (questionsAnswered === 3) displayCompletion(); //*********//
+    else {
+      setRandomIndex();
+      displayQuestion();
+      displayAnswers();
+    }
+
+    // displays feedback caption
+    quizSection.append(feedback);
+    setTimeout(() => {
+      quizSection.removeChild(feedback);
+    }, 1000);
   }
-
-  questions.splice(randomIndex,1)
-  answerChoices.splice(randomIndex,1)
-  correctAnswers.splice(randomIndex,1)
-
-  quizSection.innerHTML = "";
-
-  setRandomIndex();
-  displayQuestion();
-  displayAnswers();
-
-  quizSection.append(feedback);
-  setTimeout(() => {
-    quizSection.removeChild(feedback);
-  }, 1000);
 });
