cdb9ca0215f184f93e7db9010b8c58ba9932e47e
Rocco <roccomaniscalco2001@gmail.com>
2020-09-29 21:01:21 -0400

    gave answer buttons functionality: go to next question, display feedback

--- js/quiz-script.js
+++ js/quiz-script.js
@@ -1,36 +1,60 @@
 var quizSection = document.querySelector('[name="quiz-section"]');
 var quizHeader = document.querySelector('[name="quiz-header"]');
 
+var randomIndex = 0;
 var questions = ["how", "who", "what"];
 var answerChoices = [
   ["me", "him", "her"],
   ["ya", "ye", "yo"],
   ["here", "there", "everywhere"],
 ];
-var correct
+var correctAnswers = ["me", "ya", "here"];
 
+setRandomIndex();
+displayAnswers();
 displayQuestion();
+
+function setRandomIndex() {
+  randomIndex = Math.floor(Math.random() * questions.length);
+}
 
 function displayQuestion() {
   var question = document.createElement("h1");
-  var questionIndex = Math.floor(Math.random() * questions.length);
 
   question.setAttribute("class", "heading");
-  question.textContent = questions[questionIndex];
-  quizSection.append(question);
-  displayAnswers(questionIndex);
+  question.textContent = questions[randomIndex];
+  quizSection.prepend(question);
 }
 
-function displayAnswers(answersIndex) {
-  var answers = answerChoices[answersIndex];
-  console.log(answerChoices[answersIndex]);
+function displayAnswers() {
+  var answers = answerChoices[randomIndex];
+  console.log(answerChoices[randomIndex]);
 
   for (var i = 0; i < answers.length; i++) {
     var answer = document.createElement("button");
-    answer.setAttribute("class", "block")
+    answer.setAttribute("class", "block");
     answer.textContent = answers[i];
-    quizSection.append(answer);
- }
+    quizSection.prepend(answer);
+  }
 }
 
-quizSection.addEventListener("click", function () {});
+quizSection.addEventListener("click", function (event) {
+  var feedback = document.createElement("caption");
+
+  if (event.target.matches("button")) {
+    if (event.target.textContent === correctAnswers[randomIndex])
+      feedback.textContent = "Correct!";
+    else feedback.textContent = "Wrong!";
+  }
+
+  quizSection.innerHTML = "";
+
+  setRandomIndex();
+  displayAnswers();
+  displayQuestion();
+
+  quizSection.append(feedback);
+  setTimeout(() => {
+    quizSection.removeChild(feedback);
+  }, 1000);
+});
